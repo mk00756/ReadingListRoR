d2f8691c9d05f3c4cfaa32e353c539ccf640fc6d
Mateusz Kulinski <mk00756@surrey.ac.uk>
2017-12-11 15:01:41 +0000

    Added AJAX for destroying books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -57,6 +57,7 @@ class BooksController < ApplicationController
     @book.destroy
     respond_to do |format|
       format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.js   { flash[:notice] = "Book was successfully destroyed." }
       format.json { head :no_content }
     end
   end
