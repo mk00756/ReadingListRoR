9b566de8da8aa43dbbe0f8f797c78bef96ac0451
Mateusz Kulinski <mk00756@surrey.ac.uk>
2017-12-11 13:22:16 +0000

    Added contact form

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,5 +5,26 @@ class HomeControllerTest < ActionController::TestCase
     get :home
     assert_response :success
   end
-
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+  
+  test "should post request contect but no email" do
+    post :request_contact
+    assert_response :redirect
+    assert_not_empty flash[:alert]
+    assert_nil flash[:notice]
+  end
+  
+  test "should post request contact" do
+    post :request_contact,
+      name: "Mateusz", email: "mk00756@me.co.uk",
+      telephone: "07891234567", message: "Testing"
+      
+    assert_response :redirect
+    assert_nil flash[:alert]
+    assert_not_empty flash[:notice]
+  end
 end
