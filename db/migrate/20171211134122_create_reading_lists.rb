f96da79cdf57954dc07335362ec3a083f43682a0
Mateusz Kulinski <mk00756@surrey.ac.uk>
2017-12-11 13:41:54 +0000

    Reading List scaffold created

---
+++ db/migrate/20171211134122_create_reading_lists.rb
@@ -0,0 +1,10 @@
+class CreateReadingLists < ActiveRecord::Migration
+  def change
+    create_table :reading_lists do |t|
+      t.string :title
+      t.string :description
+
+      t.timestamps null: false
+    end
+  end
+end
