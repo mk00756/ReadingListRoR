5d72bd30c138b76f73fced75043ddffee6ecc185
mk00756 <mkulinski1998@gmail.com>
2020-11-24 22:34:04 +0000

    Upload of project files

--- test/mailers/previews/contact_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
-class ContactMailerPreview < ActionMailer::Preview
-  def contact_email
-      ContactMailer.contact_email("mk00756@me.com","Mateusz Kulinski", "078901234567", @message = "Testing")
-  end
-end
